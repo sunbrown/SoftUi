d7aaf77a37240c1d634ce2040e60abf6f849a9cb
Spencer <t.f-zhan@foxmail.com>
2019-05-10 10:02:36 +0800

    图片掩膜，修改保存图片路径，增加单选图片功能

--- U-Net/process.py
+++ U-Net/process.py
@@ -6,19 +6,16 @@ from torch import nn
 from PIL import Image
 from torchvision import transforms
 from unet import UNet
-
+from os import path as osp
 os.environ['CUDA_VISIBLE_DEVICES'] = '0, 1, 2, 3'
-
-
 def pro(path):
     n_class = 3
     model = UNet(n_channels=3, n_classes=n_class)
-    # print(model)
-    # exit()
+    print(model)
+
     model = nn.DataParallel(model, device_ids=[0])
 
     model.load_state_dict(torch.load('trainmodels.pth'))
-    # model.eval()
 
     transform = transforms.Compose([
         transforms.ToTensor(),
@@ -31,6 +28,9 @@ def pro(path):
     n = image.size()
     data = torch.ones(1, 3, n[1], n[2])
     data[0] = image
+    
+    srcim_tensor = data#保存原图
+    
     data = Variable(data.cuda(0))
     with torch.no_grad():
         output = model(data)
@@ -39,12 +39,25 @@ def pro(path):
     out_img_tensor = torch.zeros(3, n[1], n[2])
     out_img_tensor[0, pre == 2] = 1
     out_img_tensor[1, pre == 1] = 1
+    #=========================================
+    srcim_tensor = torch.reshape(srcim_tensor,(3, n[1], n[2]))
+    for i in range(H):
+        for j in range(W):
+            if out_img_tensor[0, i, j] == 1 or out_img_tensor[1, i, j] == 1:
+                        srcim_tensor[:,i,j] = 0
+    out_img_tensor = srcim_tensor + out_img_tensor
+    #待修改，
+    #==========================================
+    
+    path_filename = osp.split(path)
+    filename = path_filename[1]#[0]表示file的路径, [1]表示文件
     pil_trans = transforms.ToPILImage('RGB')
     out_img = pil_trans(out_img_tensor)
-    out_img.save('{}_result.jpg'.format(path))
-    img1 = QPixmap('{}_result.jpg'.format(path))
+    out_img.save('../../segmented_data/filename')
+    img1 = QPixmap('../../segmented_data/filename')
     return img1
-
+    
+    
 
 if __name__ == '__main__':
     path = r'D:\code\thyroid_nodule\data\img\1.2.250.1.204.5.8373722513.201611101635204868.75.dcm.jpg'
